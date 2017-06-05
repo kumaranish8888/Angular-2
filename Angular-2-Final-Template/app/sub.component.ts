5d831cb321a78904bc7a380de205de0eb3ea3101
kumaranish8888 <kumaranish8888@gmail.com>
2017-06-04 23:26:02 -0400

    New project started

---
+++ Angular-2-Final-Template/app/sub.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'sub-app',
+    template: `<h2>This is the sub Component</h2>`
+})
+
+export class SubComponent { 
+    
+ }
